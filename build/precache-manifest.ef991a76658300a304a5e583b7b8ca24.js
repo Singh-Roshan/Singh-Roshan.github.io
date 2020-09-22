049e3615f197e0de69828c2800401fec83802356
Roshan Singh <roshansingh313@gmail.com>
2020-09-21 22:15:20 -0400

    final

---
+++ build/precache-manifest.ef991a76658300a304a5e583b7b8ca24.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9846249037a0ffe92509a7cbbba15ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f682bb0fb33c7dc585c3",
+    "url": "/static/css/3.70d62d19.chunk.css"
+  },
+  {
+    "revision": "febe2aeaad45e4894af0",
+    "url": "/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "ebf3fe10bba9a66e082f",
+    "url": "/static/css/main.b892a6c8.chunk.css"
+  },
+  {
+    "revision": "fecce81c6c571915d98c",
+    "url": "/static/js/2.5c36fd95.chunk.js"
+  },
+  {
+    "revision": "f682bb0fb33c7dc585c3",
+    "url": "/static/js/3.975d6e9f.chunk.js"
+  },
+  {
+    "revision": "febe2aeaad45e4894af0",
+    "url": "/static/js/4.2ae26f9b.chunk.js"
+  },
+  {
+    "revision": "ebf3fe10bba9a66e082f",
+    "url": "/static/js/main.c6c747df.chunk.js"
+  },
+  {
+    "revision": "4b276316e0ae6ac6bee6",
+    "url": "/static/js/runtime-main.5b3fde61.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "5b83a4fa5512a327692fabf025fcf2ec",
+    "url": "/static/media/BGS.5b83a4fa.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "5b07e20c735c4ebfa29d2065035c7032",
+    "url": "/static/media/UN.5b07e20c.jpg"
+  },
+  {
+    "revision": "d38b49e76ca34104d2135413695392ff",
+    "url": "/static/media/agni.d38b49e7.jpg"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "2ac4216752233c60b0ac034e087870b6",
+    "url": "/static/media/education.2ac42167.svg"
+  },
+  {
+    "revision": "7ff4db137b6cab519a990a32c3ff6b31",
+    "url": "/static/media/google.7ff4db13.jpg"
+  },
+  {
+    "revision": "83a05b10639d238bd8c5bbab279ab911",
+    "url": "/static/media/harvard.83a05b10.png"
+  },
+  {
+    "revision": "c7462d0c09a55629af687b4052f5239e",
+    "url": "/static/media/linkedin.c7462d0c.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "e87fd5aa5b5a7db4d85f73023c707900",
+    "url": "/static/media/nyc311.e87fd5aa.jpg"
+  },
+  {
+    "revision": "3c182751328bd1e955dc41fbc80586ae",
+    "url": "/static/media/rutgers.3c182751.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "04ea8e2f8d7bf29414cf660c0c162d4f",
+    "url": "/static/media/techdata.04ea8e2f.jpg"
+  },
+  {
+    "revision": "0013b76ccf12ed511669b14bbdda64e1",
+    "url": "/static/media/york.0013b76c.png"
+  }
+]);
